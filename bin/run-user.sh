50d2984f98cac1c1b3260eae46e6ae292a1451b4
Anurag Khandelwal <anuragk@berkeley.edu>
2014-02-20 02:01:40 -0800

    Added run-user.sh

---
+++ bin/run-user.sh
@@ -0,0 +1,27 @@
+bin=`cd "$( dirname "$0" )"; pwd`
+
+ensure_dirs() {
+  	if [ ! -d "$TACHYON_LOGS_DIR" ]; then
+    	echo "TACHYON_LOGS_DIR: $TACHYON_LOGS_DIR"
+    	mkdir -p $TACHYON_LOGS_DIR
+  	fi
+}
+
+get_env() {
+  	DEFAULT_LIBEXEC_DIR="$bin"/../libexec
+  	TACHYON_LIBEXEC_DIR=${TACHYON_LIBEXEC_DIR:-$DEFAULT_LIBEXEC_DIR}
+  	. $TACHYON_LIBEXEC_DIR/tachyon-config.sh
+}
+
+# get environment
+get_env
+
+# ensure log/data dirs
+ensure_dirs
+
+MASTER_ADDRESS=$TACHYON_MASTER_ADDRESS
+if [ -z $TACHYON_MASTER_ADDRESS ] ; then
+	MASTER_ADDRESS=localhost
+fi
+
+$JAVA -cp $TACHYON_JAR -Dtachyon.home=$TACHYON_HOME $TACHYON_JAVA_OPTS succinct.test.User $MASTER_ADDRESS
